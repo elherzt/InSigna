de558ea34aeeacda0d9b4336cd4000bcf3fac127
Jesus Romero <elherzt@gmail.com>
2025-08-04 16:00:33 -0600

    add basic structure data

---
+++ InSigna/InSigna/Common/BasicSessionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSigna.Common
+{
+    /// <summary>
+    /// Basic session model for user information.
+    /// </summary>
+    public class BasicSessionModel
+    {
+        public string Provider { get; set; } 
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ InSigna/InSigna/Common/InSignaResponse.cs
@@ -0,0 +1,30 @@
+﻿using InSigna.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InSigna.Common
+{
+    public class InSignaResponse
+    {
+
+        public InSignaResponse()
+        {
+            this.TypeOfResponse = TypeOfResponse.OK;
+        }
+
+        public InSignaResponse(TypeOfResponse typeOfResponse, string message = "")
+        {
+            this.TypeOfResponse = typeOfResponse;
+            this.Message = message;
+            this.Data = null;
+        }
+
+
+        public TypeOfResponse TypeOfResponse { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public object? Data { get; set; }
+    }
+}
